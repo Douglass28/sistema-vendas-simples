70bd6a69bdf550b22f4e92a94f4923faa132c3c9
douglas souza <douglas.souza1997@hotmail.com>
2022-10-18 16:20:07 -0300

    autenticação em memória

--- src/main/java/com/example/dsevolution/teste/configs/SecurityConfig.java
+++ src/main/java/com/example/dsevolution/teste/configs/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.dsevolution.teste.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,5 +21,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("douglas")
+                .password(passwordEncoder().encode("senha123"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
