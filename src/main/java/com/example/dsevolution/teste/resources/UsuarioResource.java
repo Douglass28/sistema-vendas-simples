86a28f717bbe9a14fcea449ba331b9963999c618
douglas souza <douglas.souza1997@hotmail.com>
2022-10-20 19:22:41 -0300

    CRUD de usuarioSistema completo

--- src/main/java/com/example/dsevolution/teste/resources/UsuarioResource.java
+++ src/main/java/com/example/dsevolution/teste/resources/UsuarioResource.java
@@ -6,15 +6,14 @@ import com.example.dsevolution.teste.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -33,9 +32,32 @@ public class UsuarioResource {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping
-    ResponseEntity<List<UsuarioSistema>> listaUsuarios(){
+    public ResponseEntity<List<UsuarioSistema>> listaUsuarios(){
         List<UsuarioSistema> usuarioSistemas = service.listaUsers();
          return ResponseEntity.ok().body(usuarioSistemas);
     }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<UsuarioSistema>> buscaPorId(@PathVariable Integer id){
+         Optional<UsuarioSistema> usuarioSistema = service.busaPorId(id);
+         return ResponseEntity.ok().body(usuarioSistema);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UsuarioSistema> atualizar(@PathVariable Integer id, @RequestBody UsuarioSistema usuario){
+        UsuarioSistema usuarioSistema = service.atualizaUsuario(id, usuario);
+        return ResponseEntity.ok().body(usuarioSistema);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping(value = "/{id]")
+    public ResponseEntity<Void> deletaUsuario(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
--- src/main/java/com/example/dsevolution/teste/services/UsuarioService.java
+++ src/main/java/com/example/dsevolution/teste/services/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -19,5 +20,27 @@ public class UsuarioService {
 
     public List<UsuarioSistema> listaUsers(){
         return repository.findAll();
+    }
+
+    public Optional<UsuarioSistema> busaPorId(Integer id){
+        Optional<UsuarioSistema> usuario = repository.findById(id);
+        return usuario;
+    }
+
+    public UsuarioSistema atualizaUsuario(Integer id, UsuarioSistema usuarioSistema){
+        UsuarioSistema usuario = repository.getReferenceById(id);
+        upDate(usuario, usuarioSistema);
+        repository.save(usuario);
+        return usuario;
+    }
+
+    public void upDate(UsuarioSistema usuario, UsuarioSistema usuarioSistema){
+        usuario.setPassword(usuarioSistema.getPassword());
+        usuario.setUsername(usuarioSistema.getUsername());
+        usuario.setRoles(usuarioSistema.getRoles());
+    }
+
+    public void delete(Integer id){
+        repository.deleteById(id);
     }
 }
