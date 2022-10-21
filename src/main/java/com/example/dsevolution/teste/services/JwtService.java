505f5a0acfd329324d8425f98c2e44ebed9fc112
douglas souza <douglas.souza1997@hotmail.com>
2022-10-21 18:10:24 -0300

    Decodificando o token gerado

--- src/main/java/com/example/dsevolution/teste/services/JwtService.java
+++ src/main/java/com/example/dsevolution/teste/services/JwtService.java
@@ -2,6 +2,8 @@ package com.example.dsevolution.teste.services;
 
 import com.example.dsevolution.teste.Application;
 import com.example.dsevolution.teste.entities.UsuarioSistema;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,12 +31,38 @@ public class JwtService {
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
 
+        // Gerando Token
         return Jwts
                 .builder()
                 .setSubject(usuarioSistema.getUsername())
                 .setExpiration(data)
                 .signWith(SignatureAlgorithm.HS512, chaveSeguranca)
                 .compact();
+    }
+    // Decoficando o token gerado atraves das credenciais do usuarioSistema
+    private Claims obterClaims (String token) throws ExpiredJwtException {
+        return Jwts
+                .parser() //responsavel por codifica o token
+                .setSigningKey(chaveSeguranca)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Verificando se o token ainda é valido
+    public boolean tokenValido(String token){
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data = dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+
+        }catch (Exception e){
+            return  false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
     }
     
     public static void main (String[] args){
@@ -43,5 +71,11 @@ public class JwtService {
         UsuarioSistema usuarioSistema = new UsuarioSistema(null, "douglas", "senha123");
         String token = service.gerarToken(usuarioSistema);
         System.out.println(token);
+
+
+        boolean isTokenValido = service.tokenValido(token);
+        System.out.println("o token é valido : " + isTokenValido);
+
+        System.out.println(service.obterLoginUsuario(token));
     }
 }
